4c13487468d676b7793490c097c70160adf9d515
Andrey <asavostianiuk.exceedteam@gmail.com>
2021-08-10 18:06:44 +0300

    created rout for authorization

--- src/modules/controllers/users.controller.js
+++ src/modules/controllers/users.controller.js
@@ -27,3 +27,31 @@ module.exports.createUsers = async(req, res, next) => {
     });
   }
 };
+
+module.exports.loginUsers = async(req, res, next) => {
+  const { login, password } = req.body;
+  const users = await Users.findOne({ login })
+  
+  if (!users) {
+    res.status(404).send('Users not found');
+  }
+
+  const isPassValid = bcrypt.compareSync(password, users.password)
+
+  if (!isPassValid) {
+    res.status(400).send('Invalid password');
+  }
+
+  const token = jwt.sign({ _id: users._id }, secretKey, { expiresIn: "1h" });
+
+  if (typeof(login) === 'string' 
+      && login
+      && password
+      ) {
+    users.save().then(result1 => {
+      res.send({ token, result1 })
+    });
+  } else {
+    res.status(404).send('Sorry cant find that!');
+  };
+};
--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   allUsers,
   createUsers,
+  loginUsers
 } = require('../controllers/users.controller');
 
 router.get('/allUsers', allUsers);
 router.post('/createUsers', createUsers);
+router.post('/loginUsers', loginUsers);
 
 module.exports = router;
